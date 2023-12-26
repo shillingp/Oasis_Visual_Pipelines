1035232eda8249a902e806b4f0f3a49e77df7bc4
XGC\Peter.Shilling <p.shilling.ps@gmail.com>
2023-12-26 14:40:32 +0000

    Add subtract operation

---
+++ Oasis_Visual_Pipelines/Operations/Aggregations/SubtractNumberBlockDiagramOperation.cs
@@ -0,0 +1,31 @@
+﻿using Oasis_Visual_Pipelines.Classes;
+using Oasis_Visual_Pipelines.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oasis_Visual_Pipelines.Operations.Aggregations
+{
+    public class SubtractNumberBlockDiagramOperation : IBlockDiagramOperation
+    {
+        public int MaxInputs => int.MaxValue;
+        public string OperationTitle => "Subtract Numbers";
+
+        public BlockOperationResult ExecuteOperation(params BlockOperationResult[] inputOperations)
+        {
+            return new BlockOperationResult(additionalOperations => 
+            {
+                IEnumerable<BlockOperationResult> allOperations = inputOperations
+                    .Concat(additionalOperations);
+
+                return allOperations
+                    .Skip(1)
+                    .Aggregate(
+                        (double)allOperations.First().Result(),
+                        (total, item) => total + item.Result());
+            });
+        }
+    }
+}
