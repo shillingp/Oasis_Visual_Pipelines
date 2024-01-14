e9deef0181fd07f462473f231a5c8fe497a2a99d
XGC\Peter.Shilling <p.shilling.ps@gmail.com>
2024-01-14 13:36:58 +0000

    Update Table source to allow editing of blank table

---
+++ Oasis_Visual_Pipelines/Dialogs/EditDataTableDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Data;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Oasis_Visual_Pipelines.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for EditDataTableDialog.xaml
+    /// </summary>
+    public partial class EditDataTableDialog : UserControl
+    {
+        public EditDataTableDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DataGrid editDataGrid = ((DataGrid)sender);
+            MoveColumnAdditionColumnToEnd(editDataGrid);
+        }
+
+        public static ICommand AddNewColumnCommand => new RelayCommand<DataGrid>((dataGrid) =>
+        {
+            DataTable editingTable = (DataTable)dataGrid!.DataContext;
+            editingTable.Columns.Add(new DataColumn());
+
+            dataGrid.AutoGenerateColumns = false;
+            dataGrid.AutoGenerateColumns = true;
+
+            MoveColumnAdditionColumnToEnd(dataGrid);
+        });
+
+        private static void MoveColumnAdditionColumnToEnd(DataGrid editDataGrid)
+        {
+            DataGridTemplateColumn addNewColumn = editDataGrid.Columns
+                .OfType<DataGridTemplateColumn>()
+                .First();
+
+            addNewColumn.DisplayIndex = editDataGrid.Columns.Count - 1;
+        }
+    }
+}
--- Oasis_Visual_Pipelines/Operations/Sources/DataTableSourceBlockDiagramOperation.cs
+++ Oasis_Visual_Pipelines/Operations/Sources/DataTableSourceBlockDiagramOperation.cs
@@ -1,7 +1,11 @@
-﻿using Oasis_Visual_Pipelines.Attributes;
+﻿using CommunityToolkit.Mvvm.Input;
+using Oasis_Visual_Pipelines.Attributes;
 using Oasis_Visual_Pipelines.Classes;
+using Oasis_Visual_Pipelines.Dialogs;
 using Oasis_Visual_Pipelines.Enums;
+using Oasis_Visual_Pipelines.Functions;
 using System.Data;
+using System.Windows.Input;
 
 namespace Oasis_Visual_Pipelines.Operations
 {
@@ -11,22 +15,32 @@ namespace Oasis_Visual_Pipelines.Operations
         public override int MaxInputs => 0;
         public override string OperationTitle => "Table Source";
 
+        public DataTable resultDataTable = new DataTable();
+
+        public DataTableSourceBlockDiagramOperation()
+        {
+#if DEBUG
+            resultDataTable.Columns.Add("Index", typeof(int));
+            resultDataTable.Columns.Add("First Name", typeof(string));
+            resultDataTable.Columns.Add("Last Name", typeof(string));
+
+            resultDataTable.Rows.Add(1, "Peter", "Shilling");
+            resultDataTable.Rows.Add(2, "John", "Smith");
+            resultDataTable.Rows.Add(3, "Joe", "Bloggs");
+#endif
+        }
+
         public override BlockOperationResult ExecuteOperation(params BlockOperationResult[] inputOperations)
         {
-            return new BlockOperationResult(additionalOperations =>
-            {
-                DataTable exampleDataTable = new DataTable();
-
-                exampleDataTable.Columns.Add("Index", typeof(int));
-                exampleDataTable.Columns.Add("First Name", typeof(string));
-                exampleDataTable.Columns.Add("Last Name", typeof(string));
-
-                exampleDataTable.Rows.Add(1, "Peter", "Shilling");
-                exampleDataTable.Rows.Add(2, "John", "Smith");
-                exampleDataTable.Rows.Add(3, "Joe", "Bloggs");
-
-                return exampleDataTable;
-            });
+            return new BlockOperationResult(resultDataTable);
         }
+
+        public ICommand EditResultTableDataManuallyCommand => new RelayCommand(async () =>
+        {
+            await DialogHostFunctions.CreateAndShowDialog(
+                content: new EditDataTableDialog(),
+                dataContext: resultDataTable,
+                closeOnClickAway: true);
+        });
     }
 }
