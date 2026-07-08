397333ed8c15be43fb3382cddd6483043a2fb0ef
Peter Shilling <p.shilling.ps@gmail.com>
2026-07-08 22:02:30 +0100

    Extract DataTemplates into directory and load from XAML

--- Oasis_Pipelines.Controls.Wpf/Converters/GeneratePathGeometryConverter.cs
+++ Oasis_Pipelines.Controls.Wpf/Converters/GeneratePathGeometryConverter.cs
@@ -15,6 +15,15 @@ public class GeneratePathGeometryConverter : IMultiValueConverter
 {
     private readonly IConnectorVisualRegistry _connectorVisualRegistry;
 
+    public GeometryType GeometryPathMethod { get; set; } = GeometryType.Bezier;
+
+    public enum GeometryType
+    {
+        Bezier,
+        RightAngle,
+        Straight
+    }
+
     public GeneratePathGeometryConverter()
     {
         _connectorVisualRegistry = ControlServiceProvider.GetRequiredService<IConnectorVisualRegistry>();
@@ -22,33 +31,43 @@ public class GeneratePathGeometryConverter : IMultiValueConverter
 
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
-        if (values is not [Connection connector, ..] )
+        if (values is not [Connection connector, ..])
             return new PathGeometry();
-        
+
         _connectorVisualRegistry.TryGetConnectorNode(
             connector, ConnectionSide.Right, out ConnectorNode? leftConnectorNode);
         _connectorVisualRegistry.TryGetConnectorNode(
             connector, ConnectionSide.Left, out ConnectorNode? rightConnectorNode);
         if (leftConnectorNode is null || rightConnectorNode is null)
             return new PathGeometry();
-        
+
         Point startPoint = leftConnectorNode.GetFrameworkElementCenter().ToPoint();
         Point endPoint = rightConnectorNode.GetFrameworkElementCenter().ToPoint();
-        
-        return GenerateBezier(startPoint, endPoint);
+
+        return GeometryCalculationFunction(GeometryPathMethod)(startPoint, endPoint);
     }
 
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
     {
         throw new NotSupportedException();
     }
-    
+
+    private static Func<Point, Point, Geometry> GeometryCalculationFunction(GeometryType geometryType) =>
+        geometryType switch
+        {
+            GeometryType.Bezier => GenerateBezier,
+            GeometryType.RightAngle => GenerateRightAngle,
+            GeometryType.Straight => GenerateStraight,
+            _ => throw new ArgumentOutOfRangeException(nameof(geometryType), geometryType, null)
+        };
+
     private static Geometry GenerateBezier(Point start, Point finish)
     {
         double horizontalOffset = Math.Abs((finish - start).X);
         int bezierExtentFromBlock = (int)horizontalOffset / 2;
 
-        Point[] points = [
+        Point[] points =
+        [
             start with { X = start.X + bezierExtentFromBlock },
             finish with { X = finish.X - bezierExtentFromBlock },
             finish
@@ -61,7 +80,7 @@ public class GeneratePathGeometryConverter : IMultiValueConverter
                 new PathFigure
                 {
                     StartPoint = start,
-                    Segments = [ new PolyBezierSegment(points, true) ]
+                    Segments = [new PolyBezierSegment(points, true)]
                 }
             ]
         };
@@ -69,7 +88,8 @@ public class GeneratePathGeometryConverter : IMultiValueConverter
 
     private static Geometry GenerateRightAngle(Point start, Point finish)
     {
-        Point[] points = [
+        Point[] points =
+        [
             start with { X = start.X + Math.Abs(finish.X - start.X) / 2 },
             finish with { X = start.X + Math.Abs(finish.X - start.X) / 2 },
             finish
@@ -82,7 +102,7 @@ public class GeneratePathGeometryConverter : IMultiValueConverter
                 new PathFigure
                 {
                     StartPoint = start,
-                    Segments = [ new PolyLineSegment(points, true) ]
+                    Segments = [new PolyLineSegment(points, true)]
                 }
             ]
         };
@@ -97,7 +117,7 @@ public class GeneratePathGeometryConverter : IMultiValueConverter
                 new PathFigure
                 {
                     StartPoint = start,
-                    Segments = [ new PolyLineSegment([finish], true) ]
+                    Segments = [new PolyLineSegment([finish], true)]
                 }
             ]
         };
